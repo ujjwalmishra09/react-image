6085475679bc9a99d36843e54eca7edc7b3292ae
Ujjwal Mishra <ujjwalmishra09@gmail.com>
2017-12-05 10:14:18 +0530

    fix for placeholder.gif

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import Image from './Image';
 ReactDOM.render(
   <Image
     imageUrl={'https://media.dev.mojoreads.com/250x360/32511de9-1e1c-4d22-845a-f5678dbf374f.png'}
-    placeHolder={'/placeHolder.gif'}
+    placeHolder={'/react-image/placeHolder.gif'}
     alt={'image'}
   />,
   document.getElementById('root')
